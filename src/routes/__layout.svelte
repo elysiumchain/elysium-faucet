03102dbdb52e6143e16e8d1cb73c3fb9de158da9
dilrong <dilrong@dilrong.com>
2022-05-18 17:26:53 +0900

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    	import '../app.scss';
+		import './layout.scss';
+</script>
+
+<svelte:head>
+    <title>Elysium Faucet</title>
+    <meta name="description" content="Open Source Platform for Decentralized Applications Faucet"/>
+</svelte:head>
+
+<header>
+	<section class="site-status">
+		<div class="network-container">
+			<p class="health"/>
+			<p><strong>testnet</strong></p>
+			<p class="separator">#</p>
+			<p>1,234</p>
+		</div>
+	</section>
+	<hr/>
+	<section class="site-menu">
+		<a class="logo" href="/">
+			<img src="/images/elysium.png" alt="elysium"/>
+			<span>Elysium</span>
+		</a>
+		<nav>
+			<ul><li></li></ul>
+		</nav>
+	</section>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+</footer>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import "./index.scss"
+</script>
+
+<section>
+    <h1>Elysium Faucet</h1>
+    <input placeholder="Your Wallet Address..."/>
+    <a class="give-button">Give Me 1 Ely</a>
+</section>
