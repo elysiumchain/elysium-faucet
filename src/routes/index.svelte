78e7fb82bbb2d942e4fab8a8684124c4977864ac
dilrong <dilrong@dilrong.com>
2022-05-31 17:05:57 +0900

    feat: grecaptch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
     import "./index.scss"
-    import { ethers } from "ethers";
+    import axios from "axios"
 
+    let alert = { success: false, error: false };
     let address = "";
 
-    async function sendEly() {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner()
-        const tx = await signer.sendTransaction({
-            to: address,
-            value: ethers.utils.parseEther("1.0"),
-        });
+    async function sendEly(e: Event) {
+        e.preventDefault();
+        window.grecaptcha.ready(() => {
+            grecaptcha.execute('6Ld6szAgAAAAAFX7Bzov3dnnUNtwY59AmU9L4PkL', {action: 'submit'}).then(async (token: string) => {
+                const data = await axios.post("api.elysium.info/recaptcha");
+                if (data.data.success) {
+                    alert.success = true;
+                } else {
+                    alert.error = true;
+                }
+            });
+        })
     }
 </script>
 
 <section>
     <h1>Elysium Faucet</h1>
+    {#if alert.success}
+    <p class="success">Sucess check your wallet!</p>
+    {/if}
+    {#if alert.error}
+    <p class="error">Request failed...</p>
+    {/if}
     <input bind:value={address} placeholder="Your Wallet Address..."/>
-    <a class="give-button" on:click|once={sendEly}>Give Me 1 Ely</a>
+    <button class="give-button" on:click|once={sendEly}>Give Me 1 Ely</button>
 </section>
